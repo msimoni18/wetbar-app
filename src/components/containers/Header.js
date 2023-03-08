c0337392b04acc933308747b112eb50063e336b1
Matthew Simoni <simoni2499@gmail.com>
2023-03-07 20:09:11 -0500

    replaced secondary text in header with icon and hover text, updated TODO.txt

--- src/components/containers/Header.js
+++ src/components/containers/Header.js
@@ -1,12 +1,26 @@
-import React from "react";
+import * as React from "react";
+import { Typography } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import { HtmlTooltip } from "./tooltips/HtmlTooltip";
 
 export default function Header(props) {
   const { heading, description } = props;
 
   return (
-    <header style={ { textAlign: "center", marginBottom: "15px" } }>
-      <h1 style={ { marginBottom: "5px" } }>{heading}</h1>
-      <p style={ { marginBottom: "15px" } }>{description}</p>
+    <header style={ { marginBottom: "15px" } }>
+      <div style={ { display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "15px" } }>
+        <h1 style={ { marginRight: "1rem" } }>{heading}</h1>
+        <HtmlTooltip
+          title={ (
+            <React.Fragment>
+              <Typography>{description}</Typography>
+            </React.Fragment>
+          ) }
+          placement="right"
+        >
+          <InfoIcon fontSize="small" color="action" />
+        </HtmlTooltip>
+      </div>
       <hr />
     </header>
   );
