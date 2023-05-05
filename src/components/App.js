4559a36de5b2e9bbb7322c25bc4b6b1a3bc829d4
Matthew Simoni <simoni2499@gmail.com>
2023-05-04 21:02:40 -0400

    added lazy loading to routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-import * as React from "react";
+import React, { lazy, Suspense } from "react";
 import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 import Titlebar from "components/titlebar/Titlebar";
-import SpaceHogs from "components/routes/spacehogs/SpaceHogs";
-import Sidebar from "components/sidebar/Sidebar";
-import Archive from "components/routes/archive/Archive";
-import Cleanup from "components/routes/cleanup/Cleanup";
-import Flamingo from "components/routes/flamingo/Flamingo";
-import Plots from "components/routes/plots/Plots";
-import SpaceUtilization from "components/routes/spaceutilization/SpaceUtilization";
-import Settings from "components/routes/settings/Settings";
-// import Test from "components/routes/test/Test";
 import { ThemeProvider } from "@mui/material";
 import theme from "../theme/theme";
 import styles from "./App.module.scss";
+
+const SpaceHogs = lazy(() => import("components/routes/spacehogs/SpaceHogs"));
+const Sidebar = lazy(() => import("components/sidebar/Sidebar"));
+const Archive = lazy(() => import("components/routes/archive/Archive"));
+const Cleanup = lazy(() => import("components/routes/cleanup/Cleanup"));
+const Flamingo = lazy(() => import("components/routes/flamingo/Flamingo"));
+const Plots = lazy(() => import("components/routes/plots/Plots"));
+const SpaceUtilization = lazy(() => import("components/routes/spaceutilization/SpaceUtilization"));
+const Settings = lazy(() => import("components/routes/settings/Settings"));
+// const Test = lazy(() => import("components/routes/test/Test"));
 
 export default function App() {
   return (
@@ -22,16 +23,18 @@ export default function App() {
           <Titlebar />
           <Sidebar />
           <div className={ styles["route-body"] }>
-            <Routes>
-              <Route path="/" element={ <SpaceHogs /> } />
-              <Route path="/cleanup" element={ <Cleanup /> } />
-              <Route path="/archive" element={ <Archive /> } />
-              <Route path="/utilization" element={ <SpaceUtilization /> } />
-              <Route path="/plots" element={ <Plots /> } />
-              <Route path="/flamingo" element={ <Flamingo /> } />
-              <Route path="/settings" element={ <Settings /> } />
-              {/* <Route path="/test" element={ <Test /> } /> */}
-            </Routes>
+            <Suspense fallback={ <h1>Loading...</h1> }>
+              <Routes>
+                <Route path="/" element={ <SpaceHogs /> } />
+                <Route path="/cleanup" element={ <Cleanup /> } />
+                <Route path="/archive" element={ <Archive /> } />
+                <Route path="/utilization" element={ <SpaceUtilization /> } />
+                <Route path="/plots" element={ <Plots /> } />
+                <Route path="/flamingo" element={ <Flamingo /> } />
+                <Route path="/settings" element={ <Settings /> } />
+                {/* <Route path="/test" element={ <Test /> } /> */}
+              </Routes>
+            </Suspense>
           </div>
         </Router>
       </ThemeProvider>
